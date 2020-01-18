57a915187142f046e9025ae97c6f34e0a0b36097
Radha Devi <radha.bollineni26@gmail.com>
2020-01-18 14:12:53 -0600

    variable java file added to branch 01/17/20

---
+++ src/com/learning/VarExample.java
@@ -0,0 +1,18 @@
+package com.learning;
+
+public class VarExample {
+
+	public static void main(String[] args) {
+		
+		float weight= 137;
+		
+		System.out.println("My weight before is " + weight +" pounds");
+		
+		weight=123.3f;
+		
+		System.out.println("Present my weight is "+weight+" pounds");
+		
+
+	}
+
+}
